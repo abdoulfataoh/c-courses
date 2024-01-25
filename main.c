a7572c57d0b84971ccfdb143bd5bb479f2a0eb15
abdoul fataoh kabore <32961108+abdoulfataoh@users.noreply.github.com>
2024-01-25 18:32:37 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+struct DonneeMeteo
+{
+    int annee;
+    int mois;
+    int jour;
+    float temperature;
+    float humudite;
+    double precipitation;
+    char commentaire[128];
+};
+
+//definir une fonction qui prend comme parametre la structure DonneMeteo
+//qui remplit les information
+
+void saisirMeteo(....)
+{
+    printf("Entrez le jour")
+    //enregitrer jour
+    printf("Entrez le mois")
+    //enregitrer mois
+    //etc..
+    printf("Entrez le precipications")
+    //enregitrer precipitation
+    //etc...
+}
+
+int main(){
+   struct main
+   {
+
+   };
+   
+}
