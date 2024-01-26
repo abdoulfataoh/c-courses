7a219da87e52798e86f0ba70b899e073d7488e74
abdoulfataoh <abdoulfataoh@gmail.com>
2024-01-26 19:24:39 +0100

    define project template

--- weather.c
+++ weather.c
@@ -2,34 +2,69 @@
 
 struct DonneeMeteo
 {
-    int annee;
-    int mois;
     int jour;
+    int mois;
+    int annee;
     float temperature;
     float humudite;
     double precipitation;
-    char commentaire[128];
 };
 
-//definir une fonction qui prend comme parametre la structure DonneMeteo
-//qui remplit les information
+int menu(){
+    int choix;
+    printf("-- Application pour Agriculteurs --\n");
+    printf("1. Entrez 1 pour enregistrer les donnees de la meteo du jour\n");
+    printf("2. Entrez 2 pour pour l'analyse et interpretation des donnees la meteo\n");
+    printf("0. Entrez 0 pour quitter\n");
+    printf("Quel est votre choix: ");
+    scanf("%d", &choix);
+    
+    while (choix < 0 || choix > 2)
+    {
+        printf("Choix invalide\n");
+        printf("Quel est votre choix: ");
+        scanf("%d", &choix);
+
+    }
+    
+    return choix;
+}
+
+
+//definir une fonction qui prend comme parametre un tableau qui remplit ce tableau
+//avec les donnees de la meteo
 
 void saisirMeteo(....)
 {
-    printf("Entrez le jour")
-    //enregitrer jour
-    printf("Entrez le mois")
-    //enregitrer mois
-    //etc..
-    printf("Entrez le precipications")
-    //enregitrer precipitation
-    //etc...
+    struct DonneeMeteo donne;
+    printf("Entrez le jour: \n");
+    scanf("%d", &donne.jour);
+    printf("Entrez le mois: \n");
+    scanf("%d", &donne.mois);
+
+}
+
+// A faire
+int analyseEtInterpreation()
+{
+    // completer la fonction
 }
 
 int main(){
-   struct main
-   {
+   int choix;
+   choix = menu();
 
-   };
+   if (choix == 0){
+    printf("Merci d'avoir utiliser notre application\n");
+    printf("Aurevoir !!!\n");
+   }
+   else if (choix == 1)
+   {
+    //appeller la fonction qui permet d'enregistrer les donnees de la meteo
+   }
+   else if (choix == 2)
+   {
+    //definir et appeler la fonction qui permet de faire l'analyse
+   }
    
 }
