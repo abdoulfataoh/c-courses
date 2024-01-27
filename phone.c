2641e6a33b4f1f6b67a2f504c18680e4578413c0
abdoulfataoh <abdoulfataoh@gmail.com>
2024-01-27 17:00:20 +0100

    update template

--- phone.c
+++ phone.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-
 struct Contact
 {
     char nom[32];
     int telephone;
     char email[128];
-    char sexe;
+    char sexe[1];
 };
 
+const int TAILLE = 100;
+struct Contact repertoire[100]; // tableau de contacts
 
 int menu(){
     int choix;
@@ -36,21 +37,66 @@ int menu(){
 // A completer
 // N'oubier pas de declarer une variable global qui contiendra la liste des contact
 
-void ajouter()
+void ajouter(struct Contact tab[])
 {
     struct Contact nouveau_contact;
     printf("Quel est le nom: ");
     scanf("%s", nouveau_contact.nom);
     printf("Quel est le telephone: ");
+    scanf("%d", &nouveau_contact.telephone);
+    printf("Quel est l'email: ");
+    scanf("%s", nouveau_contact.email);
+    printf("Quel est le sexe: ");
+    scanf("%s", nouveau_contact.sexe);
+    tab[0] = nouveau_contact;
 }
+
+void afficher(struct Contact tab[], int taille)
+{
+    
+    for (int i = 0; i <  TAILLE; i++)
+    {   
+        struct Contact c;
+        c = tab[i];
+        if (c.nom != 0 c.nom) // defisser la condition pour que les cases vide ne saffiche pas
+        {
+            printf("nom = %s telephone = %d \n", c.nom, c.telephone); //complter la ligne avec toutes les information du contact
+            printf("-----------------\n");
+        }
+    }
+}
+
+//definissez la fonction la fonction rechercher qui en parametre le tableau, la taille du tableau, le nom a recherche
+
+
+// definir la fonction modifier
 
 int main(){
     int choix;
 
-    choix = menu();
-    if (choix == 1)
+    while (1)
     {
-        ajouter();
+
+        choix = menu();
+
+        if (choix == 0)
+        {
+            printf("Vous avez choisi de quitter\n");
+            printf("Merci!!\n");
+            // comment arreter le programme. peut etre exit ?
+        }
+
+        if (choix == 1)
+        {
+            ajouter(repertoire);
+        }
+
+        if (choix == 2)
+        {
+            afficher(repertoire, TAILLE);
+        }
+
+        // completer les options ici
     }
-    
+ 
 }
